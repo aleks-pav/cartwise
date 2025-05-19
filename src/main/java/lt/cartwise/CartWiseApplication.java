fcaf37044a881fcc8e8793ba8a1bac7f0eb401fc
aleks-pav <ale.pavlo@gmail.com>
2025-05-19 09:03:28 +0300

    Git init

---
+++ src/main/java/lt/cartwise/CartWiseApplication.java
@@ -0,0 +1,13 @@
+package lt.cartwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/cartwise/CartWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.cartwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
